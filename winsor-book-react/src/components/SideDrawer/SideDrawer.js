1bf89d54db3a37ee2f76d55cf072acbe2233893f
hsun <hsun@owneriq.com>
2019-09-06 18:03:43 -0400

    add logo

--- winsor-book-react/src/components/SideDrawer/SideDrawer.js
+++ winsor-book-react/src/components/SideDrawer/SideDrawer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import './SideDrawer.css';
+import math_logo from '../../svg/square-root-alt-solid.svg';
+import biology_logo from '../../svg/dna-solid.svg';
+import chemistry_logo from '../../svg/flask-solid.svg';
+import english_logo from '../../svg/pen-fancy-solid.svg';
+import history_logo from '../../svg/landmark-solid.svg';
 
 const sideDrawer = props => {
   let drawerClasses = 'side-drawer';
@@ -11,13 +16,19 @@ const sideDrawer = props => {
     <nav className={drawerClasses}>
       <ul>
         <li>
-          <a href="/">Home</a>
+          <a href="/books/English"><img src={english_logo} className="subject-logo" alt="logo" /></a>
         </li>
         <li>
-          <a href="/books/Chemistry">Books Chemistry</a>
+          <a href="/books/Biology"><img src={biology_logo} className="subject-logo" alt="logo" /></a>
         </li>
         <li>
-          <a href="/books/Biology">Books Biology</a>
+          <a href="/books/Math"><img src={math_logo} className="subject-logo" alt="logo" /></a>
+        </li>
+        <li>
+          <a href="/books/Chemistry"><img src={chemistry_logo} className="subject-logo" alt="logo" /></a>
+        </li>
+        <li>
+          <a href="/books/History"><img src={history_logo} className="subject-logo" alt="logo" /></a>
         </li>
       </ul>
     </nav>
