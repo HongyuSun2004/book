bc7151efb65992145c993c37edb23e8e90e94983
hsun <hsun@owneriq.com>
2019-09-07 18:56:07 -0400

    add book for sell

--- winsor-book-rest/index.js
+++ winsor-book-rest/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const booksRoute = require('./routes/books');
 const bookRoute = require('./routes/book');
+const book4sellRoute = require('./routes/book4sell');
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 
 app.use('/books', booksRoute);
 app.use('/book', bookRoute);
+app.use('/book4sell', book4sellRoute);
 
 app.get('/', (req,res) => {
     res.send('Hello')
---
+++ winsor-book-rest/routes/book4sell.js
@@ -0,0 +1,73 @@
+const experss = require('express');
+const router = experss.Router();
+const cores = require('cors');
+const mysql = require('mysql');
+
+const BOOK_SQL = "select * from book_market where role = 'SELL' limit 10";
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'emma',
+    password: 'Winsor123',
+    database: 'book'
+});
+
+connection.connect(err=>{
+    if(err){
+        return err;
+    }
+});
+     
+router.get('/', (req,res) => {
+    connection.query(BOOK_SQL, (err, result) => {
+        if(err){
+            return res.send(err);
+        }else{
+            return res.json({
+                book4sell:result
+            })
+        }
+    });
+});
+
+router.get('/:id', (req,res) => {
+    let sql = `select * from book_market where role = 'SELL' and book_id = '${req.params.id}'`;
+    connection.query(sql, (err, result) => {
+        if(err){
+            return res.send(err);
+        }else{
+            return res.json({
+                book4sell:result
+            })
+        }
+    });
+})
+
+router.post('/', (req,res) => {
+    console.log(req.body);
+
+    let values = req.body.price.split("$")
+    let price = 0;
+    if(values.length > 1){
+        price = parseFloat(values[1]);
+    }else{
+        price = parseFloat(values[0]);
+    }
+    
+    console.log(req.body.price);
+    console.log(price);
+
+    let sql = `insert into book_market (book_id, user_id, price, book_condition) values (${req.body.book_id}, ${req.body.user_id},${price},'${req.body.condition}')`
+    connection.query(sql, (err, result) => {
+        if(err){
+            return res.send(err);
+        }else{
+            console.log(result)
+            return res.json({
+                book4sell:result
+            })
+        }
+    });
+})
+
+module.exports = router;
